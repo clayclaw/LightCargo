1471891f9baecbd0c73d4ac78bb6cf5f6b9cce08
clayclaw <nope>
2023-12-03 02:13:05 +0800

    add kotlin-script-runtime dependency to fatjar

--- cargo-kts-engine/build.gradle.kts
+++ cargo-kts-engine/build.gradle.kts
@@ -10,6 +10,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies")
     implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies-maven")
     implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable")
+
+    implementation("org.jetbrains.kotlin:kotlin-script-runtime")
 }
 repositories {
     mavenCentral()
